9e8cdb9a03367c01a402ddbff6c06156bcd74b45
cjy <cjyoun95@naver.com>
2022-06-08 15:41:56 +0900

    퀵정렬

--- src/main/kotlin/quicksort/QuickSort.kt
+++ src/main/kotlin/quicksort/QuickSort.kt
@@ -2,14 +2,14 @@
 // 퀵정렬을 이용해서 해결
 fun main(){
     println("시작")
-    val num: Int = 118372
+    val num: Int = 118375
     println(solution(num))
 }
 
 fun solution(n: Int): String {
     var answer:String = ""  // 정렬된 정수
     var num = n     // 받은 정수
-    var numList = arrayListOf<Int>()    // 받은 정수를 배열에 나눠 넣기 위한 리스트
+    val numList = arrayListOf<Int>()    // 받은 정수를 배열에 나눠 넣기 위한 리스트
 
     // 정수를 10의 자리 마다 끊어서 넣기
     while(num > 0) {
@@ -33,22 +33,24 @@ fun quickMerge(arr: ArrayList<Int>): ArrayList<Int>{
         return arr
     }
 
-    var pivot = arr.removeFirst()   // index의 처음 값을 pivot 설정 (비교대상 설정)
+    val pivot = arr[0]   // index의 처음 값을 pivot 설정 (비교대상 설정)
+    arr.removeFirst()    // pivot값을 리스트에서 제거
 
-    var left = arrayListOf<Int>()   // 왼쪽 리스트
-    var right = arrayListOf<Int>()  // 오른쪽 리스트
+    val left = arrayListOf<Int>()   // 왼쪽 리스트
+    val right = arrayListOf<Int>()  // 오른쪽 리스트
 
     // pivot 값과 비교해서 작으면 왼쪽 크면 오른쪽 리스트로 이동  -> 이렇게하면 오름차순 반대로 하면 내림차순
-    for(i in arr.indices){
+    for(i in 0 until arr.size){
         if(arr[i] < pivot){
             left.add(arr[i])
         } else{
             right.add(arr[i])
         }
+
     }
 
     // 리턴해줄 리스트
-    var mergeList = arrayListOf<Int>()
+    val mergeList = arrayListOf<Int>()
 
     // 재귀함수를 통해 정렬된 left + pivot + right 순으로 merge
     mergeList.addAll(quickMerge(left))
