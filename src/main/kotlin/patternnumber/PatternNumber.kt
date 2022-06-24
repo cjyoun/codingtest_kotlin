50132ce179f13aea29b814b469dfaddb760443e0
cjy <cjyoun95@naver.com>
2022-06-24 15:21:32 +0900

    패턴에 따라 수 증가 / 또다른 답

--- src/main/kotlin/patternnumber/PatternNumber.kt
+++ src/main/kotlin/patternnumber/PatternNumber.kt
@@ -14,7 +14,13 @@ fun solution(x: Int, n: Int): LongArray {
         answer[i] = num
         num += x
     }
+    return answer
+}
 
-
+// 또다른 답변 : LongArray를 선언할 때 크기과 값을 넣어주는 부분에서 값부분에 it을 사용하여 값을 넣어줌 (it은 인덱스 번호를 의미)
+fun solution2(x: Int, n: Int): LongArray {
+    val answer = LongArray(n) {
+        (x.toLong() * (it + 1))
+    }
     return answer
 }
